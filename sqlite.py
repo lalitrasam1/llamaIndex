9916a842946317caf4667ef647df5cf514f78719
rasamlalit9gmail.com <Jio@1234>
2024-11-23 19:05:37 +0530

    sql app

---
+++ sqlite.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+conn=sqlite3.connect("student.db")
+
+cursor = conn.cursor()
+
+table_info="""
+Create table STUDENT(NAME VARCHAR(25),CLASS VARCHAR(25),
+SECTION VARCHAR(25),MARKS INT);
+
+"""
+
+cursor.execute(table_info)
+
+cursor.execute('''Insert Into STUDENT values('Krish','Data Science','A',90)''')
+cursor.execute('''Insert Into STUDENT values('Sudhanshu','Data Science','B',100)''')
+cursor.execute('''Insert Into STUDENT values('Darius','Data Science','A',86)''')
+cursor.execute('''Insert Into STUDENT values('Vikash','DEVOPS','A',50)''')
+cursor.execute('''Insert Into STUDENT values('Dipesh','DEVOPS','A',35)''')
+
+print("The inserted records are: ")
+
+data=cursor.execute('''Select * from STUDENT''')
+
+for row in data:
+    print(row)
+
+conn.commit()
+conn.close()
